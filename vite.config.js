bf902e4672e656dea40555dfb2f4bc39354dd9b6
Wh04m7 <slipknot.rulit3@gmail.com>
2025-03-21 16:18:56 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
   root: './src',
-  base: '/',
+  base: '/', // /coca/ для деплоя на гитхаб
   server: {
     port: 3000,
     open: true,
